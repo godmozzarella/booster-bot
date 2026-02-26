26c73acbdcd7cd855fdf6b9750911f72009819f1
Mozzarella <kir.mozzarella@gmail.com>
2026-02-26 20:59:23 +0300

    Initial commit

---
+++ src/main/java/com/example/buster_bot/BusterBotApplication.java
@@ -0,0 +1,13 @@
+package com.example.buster_bot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BusterBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BusterBotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/buster_bot/BusterBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.buster_bot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BusterBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
